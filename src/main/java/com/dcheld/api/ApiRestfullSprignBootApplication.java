335366f2e2f908e9e12b2a313b049663fd823f8a
Douglas Held <douglas.held@dextra-sw.com>
2021-06-21 15:21:29 -0300

    First commit

---
+++ src/main/java/com/dcheld/api/ApiRestfullSprignBootApplication.java
@@ -0,0 +1,13 @@
+package com.dcheld.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfullSprignBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestfullSprignBootApplication.class, args);
+    }
+
+}
