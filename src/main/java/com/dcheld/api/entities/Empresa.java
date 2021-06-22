788745be7ec5b500085683bab42b385cbd4c84a3
Douglas Held <douglas.held@dextra-sw.com>
2021-06-22 13:31:38 -0300

    Add first migration using Flyway core

--- src/main/java/com/dcheld/api/entities/Empresa.java
+++ src/main/java/com/dcheld/api/entities/Empresa.java
@@ -1,4 +1,84 @@
 package com.dcheld.api.entities;
 
-public class Empresa {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+    private static final long serialVersionUID = -4962944636710316535L;
+
+    private Long id;
+    private String razaoSocial;
+    private String cnpj;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+
+    public Empresa() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "razao_social", nullable = false)
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    @Column(name = "cnpj", nullable = false)
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Column(name = "data_criacao", nullable = false)
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Column(name = "data_atualizacao", nullable = false)
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+                + ", dataAtualizacao=" + dataAtualizacao + "]";
+    }
 }
